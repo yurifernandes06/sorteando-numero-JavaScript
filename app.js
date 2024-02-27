dd24af19c7c5e9c3161f462d945d50477fd99784
Yuri Fernandes <yurifs2006@gmail.com>
2024-02-27 13:53:56 -0300

    projeto JavaScript, HTML e CSS finalizado.

---
+++ app.js
@@ -0,0 +1,54 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+      }
+
+    if (quantidade >= de) {
+        alert('Campo "Quantidade de números" deve ser inferior aos números que podem ser sorteados. Verifique!');
+        return;
+      }
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de,ate);
+        }
+
+        sorteados.push(numero);}
+
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min,max) {
+   return Math.floor(Math.random() * (max - min + 1)) +min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado')
+        botao.classList.add('container__botao')
+    } else {
+        botao.classList.add('container__botao-desabilitado')
+        botao.classList.remove('container__botao')
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao();
+}
